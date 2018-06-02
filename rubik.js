1dbef1c8feeb8ece85ccb171552c54aac1b46008
GeorgeZJChen <35523752+GeorgeZJChen@users.noreply.github.com>
2018-06-03 00:07:43 +0100

    Update rubik.js

--- rubik.js
+++ rubik.js
@@ -15,9 +15,11 @@
       return M.join(' ');
     }
 
-  document.body.addEventListener('touchmove', function(e){
-    e.preventDefault()
-  })
+    window.onload=function () {
+            document.addEventListener('touchmove', function(e){
+              e.preventDefault()
+            }, {passive: false})
+        }
   
   var id_prefix = 'isb9qalC_'
   var rotate_path = []
